49ea611a6d623ad2aa50308e6e3814c419478649
shoeiedibistl <shoeiedibistl@mail.ru>
2024-06-22 16:06:00 +0300

    preloader anim

--- src/js/components/stopAnim.js
+++ src/js/components/stopAnim.js
@@ -2,26 +2,20 @@ export const stopAnim = () => {
   const stopBtn = document.querySelector('button.stop-anim');
   const preloader = document.querySelector('.preloader');
   const preloaderItems = preloader.querySelectorAll('[data-preloader-item]');
-
-  console.log(preloaderItems);
-
-  //  stopBtn.addEventListener('click', () => {
+  const loadingScale = preloader.querySelector('.preloader__scale-loading');
+  let counter = 0;
 
   document.addEventListener('DOMContentLoaded', function () {
-    console.log('stop anim');
-
     preloaderItems.forEach((item) => {
       item.style.animationIterationCount = '1';
 
       Promise.all(
         item.getAnimations({ subtree: true }).map((animation) => animation.finished)
-      ).then(() => item.classList.add('fix'));
-
-      //  item.getAnimations.forEach((anim, i, arr) => {
-      //    if (i == 1) anim.onfinish = () => item.classList.add('fix');
-      //  });
-
-      //item.addEventListener('transitionend', item.classList.add('fix'));
+      ).then(() => {
+        item.classList.add('fix');
+        counter += 1;
+        if (counter === preloaderItems.length) loadingScale.classList.add('loaded');
+      });
     });
   });
 };
