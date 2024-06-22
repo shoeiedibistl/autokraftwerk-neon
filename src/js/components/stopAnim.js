e59747c00584828d09476c99732b2c204d288ca3
shoeiedibistl <shoeiedibistl@mail.ru>
2024-06-23 01:05:48 +0300

    preloader

--- src/js/components/stopAnim.js
+++ src/js/components/stopAnim.js
@@ -1,8 +1,9 @@
 export const stopAnim = () => {
-  const stopBtn = document.querySelector('button.stop-anim');
   const preloader = document.querySelector('.preloader');
   const preloaderItems = preloader.querySelectorAll('[data-preloader-item]');
   const loadingScale = preloader.querySelector('.preloader__scale-loading');
+  const myPage = document.querySelector('body.body');
+  const animOnLoad = document.querySelectorAll('[data-page-loaded="start-anim"]');
   let counter = 0;
 
   document.addEventListener('DOMContentLoaded', function () {
@@ -14,7 +15,16 @@ export const stopAnim = () => {
       ).then(() => {
         item.classList.add('fix');
         counter += 1;
-        if (counter === preloaderItems.length) loadingScale.classList.add('loaded');
+        if (counter === preloaderItems.length) {
+          loadingScale.classList.add('loaded');
+          setTimeout(() => {
+            myPage.classList.add('loaded');
+            //            preloader.style.opacity = '0';
+            setTimeout(() => {
+              animOnLoad.forEach((item) => item.classList.remove('pause-animation'));
+            }, 300);
+          }, 3000);
+        }
       });
     });
   });
